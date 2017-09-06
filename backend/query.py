e29c12d6f606d29061985df35e876e90ba9b6f19
Sourabh Lal <sourabhlal@gmail.com>
2017-09-06 15:32:02 +0200

    skeleton code

---
+++ backend/query.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def parseQuery(query):
+	"""
+	Parses query to identify keywords
+	:param query: query entered as a string
+	Returns: list of keywords, relevant database
+	"""
+	return keywords, database
+
+def queryDB(keywords, database):
+	"""
+	Queries DB for values
+	:param keywords: list of keywords relevant to query
+	:parm query: name of database
+	Returns: value, responseID
+	"""
+	return values, responseID
+
+def processResponse(values, responseID):
+	"""
+	Prettufy response and insert values
+	:param keywords: list of keywords relevant to query
+	:parm query: name of database
+	Returns: response
+	"""
+	return response
