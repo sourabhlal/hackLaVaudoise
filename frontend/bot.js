437bde7a28bfd780563258bcaa57b956c939952c
Sourabh Lal <sourabhlal@gmail.com>
2017-09-06 23:00:55 +0200

    speech works oh yeah

--- frontend/bot.js
+++ frontend/bot.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
 	try {
 		var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 		var recognition = new SpeechRecognition();
+		recognition.lang = 'fr-FR';
 		}
 		catch(e) {
 			console.error(e);
@@ -13,28 +14,11 @@ $( document ).ready(function() {
 
 	console.log(recognition)
 
-	recognition.onstart = function() { 
-		instructions.text('Voice recognition activated. Try speaking into the microphone.');
-	}
 
-	recognition.onspeechend = function() {
-		instructions.text('You were quiet for a while so voice recognition turned itself off.');
-	}
-
-	recognition.onerror = function(event) {
-		if(event.error == 'no-speech') {
-			instructions.text('No speech was detected. Try again.');  
-		};
-	}
 	recognition.onresult = function(event) {
 		var current = event.resultIndex;
   		var transcript = event.results[current][0].transcript;
-		noteContent += transcript;
-		noteTextarea.val(noteContent);
-
-		console.log(current)
 		console.log(transcript)
-		console.log(noteContent)
 	}
 
 
